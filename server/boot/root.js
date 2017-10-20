e1f65144df629b03bc7459d643d3e486d67dad29
Eric Brovko <Eric Brovko>
2017-10-20 18:22:46 +0300

    implantation save data from xlsx file

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,11 +3,9 @@
 const controller = require('../../common/controllers/main');
 
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  // router.get('/', server.loopback.status());
-  router.get('/initialData.xlsx', controller.initialData);
+  const router = server.loopback.Router();
 
+  router.get('/initialData.xlsx', controller.initialData);
 
   server.use(router);
 };
